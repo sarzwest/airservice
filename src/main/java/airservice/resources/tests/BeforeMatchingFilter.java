6e8bb2ca7c1968a71460681c5dfe0889ca16ea46
sarzwest <jiratom@seznam.cz>
2014-11-11 21:10:06 +0100

    BeforeMatching

--- src/main/java/airservice/resources/tests/BeforeMatchingFilter.java
+++ src/main/java/airservice/resources/tests/BeforeMatchingFilter.java
@@ -7,9 +7,15 @@
 package airservice.resources.tests;
 
 import java.io.IOException;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 
 /**
@@ -18,11 +24,24 @@ import javax.ws.rs.ext.Provider;
  */
 @Provider
 @PreMatching
-public class BeforeMatchingFilter implements ContainerRequestFilter{
+@SessionScoped
+public class BeforeMatchingFilter implements ContainerRequestFilter, Serializable{
 
+    @Context
+    private transient HttpServletRequest servletRequest;
+    private int s;
     @Override
     public void filter(ContainerRequestContext crc) throws IOException {
         System.out.println("Before matching filter: ");
+        HttpSession session = servletRequest.getSession(true);
+        Integer attribute = (Integer)session.getAttribute("key");
+        if(attribute == null){
+            attribute = -1;
+        }
+        System.out.println(s);
+        s = attribute;
+        System.out.println(s);
+        session.setAttribute("key", attribute + 1);
     }
     
 }
